8a7252d8b771d5b8c8be55091d365ebe6cd65c2b
Charles Peden <ccbpeden@warpmail.net>
2017-03-17 12:59:40 -0700

    refactor to ensure all data manipulation happens on 'back side'

--- js/drSearch-interface.js
+++ js/drSearch-interface.js
@@ -6,20 +6,14 @@ var displaySuccess = function(searchResult)
   var displayResult = "";
   var complete = false;
 
-  if(searchResult.meta.count === 0)
+  if(searchResult.length === 0)
   {
     displayResult +="<p>No results matched your criteria</p>";
     complete = true;
-  } else if (searchResult.meta.count > 0 && searchResult.meta.skip === 0)
+  } else if (searchResult.length > 0)
   {
-    displayResult +="<p>" + searchResult.meta.count + " results matched your criteria.</p>";
-  }
-  if(searchResult.meta.count > 0)
-  {
+    displayResult +="<p>" + searchResult.length + " results matched your criteria.</p>";
     displayResult +="Some doctor Info here!";
-    searchResult.data.forEach(function(doctor){
-      sievedDoc = outputSieve(doctor);
-    });
     complete = true;
   }
 
@@ -29,51 +23,6 @@ var displaySuccess = function(searchResult)
   }
 };
 
-var outputSieve = function (doctor)
-{
-  console.log(doctor);
-  var output = [];
-  if (doctor.profile.image_url)
-  {
-    output.push(doctor.profile.image_url);
-  } else {
-    ouput.push("./../img/genericdricon.jpg");
-  }
-
-  if (doctor.profile.last_name)
-  {
-    output.push(doctor.profile.last_name);
-  } else
-  {
-    output.push("No last name provided");
-  }
-
-  if (doctor.profile.first_name)
-  {
-    output.push(doctor.profile.first_name);
-  } else
-  {
-    output.push("No first name provided");
-  }
-  var specialties = "";
-  doctor.specialties.forEach(function(specialty){
-    if (specialty.name)
-    {
-      specialties += specialty.name + ", ";
-    }
-  });
-  specialties = specialties.slice(0,-2);
-  output.push(specialties);
-
-  if (doctor.ratings.length != 0)
-  {
-    output.push(doctor.ratings.image_url_small)
-  } else
-  {
-    output.push("No rating available");
-  }
-  console.log(output);
-};
 
 var displayFailure = function(searchResult)
 {
--- js/drSearch.js
+++ js/drSearch.js
@@ -16,15 +16,70 @@
 
  DrSearch.prototype.getResults = function (page, doneCallBack, failCallBack) {
    var apiRequest = this.constructURL(this.medicalIssue); //change input for more complex queries later
+   var boundSieve = this.outputSieve.bind();
    $.get(apiRequest)
    .done(function(response){
      console.log(response);
-     doneCallBack(response);
+     var result = [];
+     var output = [];
+     response.data.forEach(function(doctor){
+       result.push(doctor);
+     });
+     result.forEach(function(doctor){
+        var doc = boundSieve(doctor);
+        output.push(doc);
+     });
+     console.log(output);
+     doneCallBack(output);
    })
    .fail(function(response){
      console.log(response);
      failCallBack(response);
    });
  };
+
+ DrSearch.prototype.outputSieve = function (doctor){
+    var output = [];
+    if (doctor.profile.image_url)
+    {
+      output.push(doctor.profile.image_url);
+    } else {
+      ouput.push("./../img/genericdricon.jpg");
+    }
+
+    if (doctor.profile.last_name)
+    {
+      output.push(doctor.profile.last_name);
+    } else
+    {
+      output.push("No last name provided");
+    }
+
+    if (doctor.profile.first_name)
+    {
+      output.push(doctor.profile.first_name);
+    } else
+    {
+      output.push("No first name provided");
+    }
+    var specialties = "";
+    doctor.specialties.forEach(function(specialty){
+      if (specialty.name)
+      {
+        specialties += specialty.name + ", ";
+      }
+    });
+    specialties = specialties.slice(0,-2);
+    output.push(specialties);
+
+    if (doctor.ratings.length !== 0)
+    {
+      output.push(doctor.ratings.image_url_small);
+    } else
+    {
+      output.push("No rating available");
+    }
+    return output;
+  };
 
  exports.drSearchModule = DrSearch;
